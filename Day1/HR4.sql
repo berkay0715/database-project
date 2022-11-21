aacd6f5388720c3a18c4488ead97c277677189ff
Berkay <berkay.s.celik@gmail.com>
2022-11-21 19:04:44 +0100

    K

--- Day1/HR4.sql
+++ Day1/HR4.sql
@@ -5,6 +5,7 @@ select distinct JOB_ID from EMPLOYEES;
 select avg(SALARY) from EMPLOYEES
 where JOB_ID = 'IT_PROG';
 
+--get me average salary for each job_id
 select JOB_ID,avg(SALARY),count(*),sum(SALARY)
 from EMPLOYEES
 group by JOB_ID;
@@ -14,3 +15,37 @@ select JOB_ID,avg(SALARY),count(*),sum(SALARY)
 from EMPLOYEES
 group by JOB_ID
 having avg(SALARY) >5000;
+
+--how to find employees information of who is making highest salary in the company
+select * from EMPLOYEES
+order by SALARY desc;
+
+--get me highest salary
+select max(SALARY) from EMPLOYEES;
+
+--highest salary employee information
+select * from EMPLOYEES
+where SALARY =24000;
+
+--subquery solution in one shot
+select * from EMPLOYEES
+where SALARY =(select max(SALARY) from EMPLOYEES);
+
+--finding second highest salary
+--first get highest salary
+select max(SALARY) from EMPLOYEES
+--highest salary after 24K
+where SALARY <24000;
+
+--one shot combining two queries
+select max(SALARY) from EMPLOYEES
+where SALARY < (select max(SALARY) from EMPLOYEES);
+
+--employee information of who is making second highest salary
+select * from EMPLOYEES
+where SALARY = (select max(SALARY) from EMPLOYEES
+                where SALARY < (select max(SALARY) from EMPLOYEES));
+
+
+
+
